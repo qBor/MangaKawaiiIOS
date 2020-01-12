df3989aee6ab053053e8b93e043c4abdbd32c29d
kevxxf <43086534+qBor@users.noreply.github.com>
2020-01-12 21:20:34 +0100

    First Push

---
+++ MangaKawaii/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MangaKawaii
+//
+//  Created by Kevin Fosse on 12/01/2020.
+//  Copyright © 2020 Kevin Fosse. All rights reserved.
+//
+
+import Foundation
--- MangaKawaii/ViewController.swift
+++ MangaKawaii/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import WebKit
+import Floaty
 
 class ViewController: UIViewController {
 
+    // Déclaration floaty
+    @IBOutlet weak var floaty: Floaty!
+    
+    // Déclaration WKWeb
+    
+    @IBOutlet weak var webView: WKWebView!
+    @objc
+    func refreshWebView(_ sender: UIRefreshControl) {
+        webView?.reload()
+        sender.endRefreshing()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        // Bar
+        
+        floaty.addItem(title:"Paramètres", handler: {_ in
+            
+            self.performSegue(withIdentifier: "gotosettings", sender: self)
+            
+        })
+        self.view.addSubview(floaty)
+
+      
+        
+        // WebView qui permet l'affichage du site
+        let url = URL(string: "https://www.mangakawaii.to")
+        let urlRequest = URLRequest(url: url!)
+        self.webView.load(urlRequest)
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshWebView(_:)), for: UIControl.Event.valueChanged)
+        webView.scrollView.addSubview(refreshControl)
+        webView.scrollView.bounces = true
     }
-
-
+    
 }
 
