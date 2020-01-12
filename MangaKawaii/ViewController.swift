b2f523e97e923233fdb784f27b620859e83742fb
kevxxf <43086534+qBor@users.noreply.github.com>
2020-01-12 18:11:14 +0100

    Initial Commit

---
+++ MangaKawaii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MangaKawaii
+//
+//  Created by Kevin Fosse on 12/01/2020.
+//  Copyright © 2020 Kevin Fosse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
